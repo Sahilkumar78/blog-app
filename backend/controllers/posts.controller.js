a5d12358935ad2e85e37b4032030f39bed50993e
SahilKumar78 <sahilabcd661@gmail.com>
2026-06-23 22:18:26 +0530

    post controller partially done

--- backend/controllers/posts.controller.js
+++ backend/controllers/posts.controller.js
@@ -51,6 +51,56 @@ import { asyncHandler } from "../utils/asyncHandler";
 
  // update blog 
 
+ const updatePost = asyncHandler(async (req, res) => {
+       
+
+    // find post by id
+        const {id} = req.params;
+
+        // check post exist
+        const post = await Post.findById(id);
+
+        if(!post){
+             throw new ApiError(404, "post not found");
+        }
+
+
+        // update title content if present
+       
+        if(post.author.toString() !== req.user._id){
+             throw new ApiError(403 , "unauthorized access");
+        }
+
+        const {title, content} = req.body;
+         
+        post.title = title || post.title;
+        post.content = content || post.content
+
+        // if new image uploaded
+          // replace coudinary link to db
+
+          if(req.file){
+             const uploadedImage = await cloudinary.uploader.upload(req.file.path,
+                {
+                     folder: "posts",
+                }
+             )
+             post.image = uploadedImage.secure_url
+          }
+
+          // save blog
+
+          await post.save();
+
+
+          // return response
+
+          return res 
+                 .status(200)
+                 .json(new ApiResponse(200, post, "post updated successfully"));
+
+       
+ })
 
  // read blog
 
@@ -62,5 +112,6 @@ import { asyncHandler } from "../utils/asyncHandler";
 
 
  export {
-       createPost
+       createPost,
+       updatePost,
  }
